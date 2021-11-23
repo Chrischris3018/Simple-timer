c16f7b49abcc4d37be6625948961cf4bab7aae06
Chrischris3018 <chrischrischris3018@yahoo.com>
2021-11-23 01:52:52 +0000

    big progress

--- src/js/component/SecondsCounter.jsx
+++ src/js/component/SecondsCounter.jsx
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
+import PropTypes from "prop-types";
 
-const SecondsCounter = () => {
-    return (
-     <div></div>   
-    )
+function SecondsCounter(props) {
+	return (
+		<div className="clock">
+			<div className="tensThousands">{props.tenThousandSeconds}</div>
+			<div className="thousands">{props.thousandSeconds}</div>
+			<div className="hundreds">{props.hundredSeconds}</div>
+			<div className="tens">{props.tenSeconds}</div>
+			<div className="ones">{props.oneSeconds}</div>
+		</div>
+	);
 }
+SecondsCounter.propTypes = {
+	tenThousandSeconds: PropTypes.number,
+	thousandSeconds: PropTypes.number,
+	hundredSeconds: PropTypes.number,
+	tenSeconds: PropTypes.number,
+	oneSeconds: PropTypes.number
+};
 
-export default SecondsCounter;
+let time = 0;
+setInterval(function() {
+	const tensThousands = Math.floor(time / 10000);
+	const thousands = Math.floor(time / 1000);
+	const hundreds = Math.floor(time / 100);
+	const tens = Math.floor(time / 10);
+	const ones = Math.floor(time / 1);
+	time++;
+}, 1000);
+export default SecondsCounter.jsx;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
+import SecondsCounter from "./SecondsCounter.jsx";
+import "../../styles/index.scss";
 
 //create your first component
 
 const Home = () => {
-	const [time, setTime] = useState(0);
-	const incrementTime = () => {};
-	let clock = setInterval(clock, 1000);
-	console.log(clock);
-	return <div>{time}</div>;
+	let time = 0;
+	setInterval(function() {
+		const tensThousands = Math.floor(time / 10000);
+		const thousands = Math.floor(time / 1000);
+		const hundreds = Math.floor(time / 100);
+		const tens = Math.floor(time / 10);
+		const ones = Math.floor(time / 1);
+		time++;
+	}, 1000);
+	return (
+		<>
+			<SecondsCounter
+				tenThousandSeconds={tensThousands}
+				thousandSeconds={thousands}
+				hundredSeconds={hundreds}
+				tenSeconds={tens}
+				oneSeconds={ones}
+			/>
+		</>
+	);
 };
-
-React.useEffect(Home, []);
 
 export default Home;
