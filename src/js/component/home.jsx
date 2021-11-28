451472d29efcd7063d200a039686bdf7ac49549f
Chrischris3018 <chrischrischris3018@yahoo.com>
2021-11-28 01:56:24 +0000

    done

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,31 +1,46 @@
-import React, { useState } from "react";
-import SecondsCounter from "./SecondsCounter.jsx";
+import React, { useEffect, useState } from "react";
 import "../../styles/index.scss";
 
 //create your first component
 
 const Home = () => {
-	let time = 0;
-	setInterval(function() {
-		const hundredThousands = Math.floor(time / 100000);
-		const tensThousands = Math.floor(time / 10000);
-		const thousands = Math.floor(time / 1000);
-		const hundreds = Math.floor(time / 100);
-		const tens = Math.floor(time / 10);
-		const ones = Math.floor(time / 1);
-		time++;
-	}, 1000);
+	const [counter, setCounter] = useState(0);
+	const [counter2, setCounter2] = useState(0);
+	const [counter3, setCounter3] = useState(0);
+	const [counter4, setCounter4] = useState(0);
+	const [counter5, setCounter5] = useState(0);
+	const [counter6, setCounter6] = useState(0);
+	useEffect(() => {
+		setInterval(() => {
+			setCounter(counter => counter + (1 % 10));
+		}, 1000);
+		setInterval(() => {
+			setCounter2(counter2 => counter2 + 1);
+		}, 10000);
+		setInterval(() => {
+			setCounter3(counter3 => counter3 + 1);
+		}, 100000);
+		setInterval(() => {
+			setCounter4(counter4 => counter4 + 1);
+		}, 1000000);
+		setInterval(() => {
+			setCounter5(counter5 => counter5 + 1);
+		}, 10000000);
+		setInterval(() => {
+			setCounter6(counter6 => counter6 + 1);
+		}, 100000000);
+	}, []);
+
 	return (
-		<>
-			<SecondsCounter
-				hundredThousandsSeconds={hundredThousands}
-				tenThousandSeconds={tensThousands}
-				thousandSeconds={thousands}
-				hundredSeconds={hundreds}
-				tenSeconds={tens}
-				oneSeconds={ones}
-			/>
-		</>
+		<div>
+			<i className="far fa-clock"></i>
+			<h1>{counter6 % 10}</h1>
+			<h1>{counter5 % 10}</h1>
+			<h1>{counter4 % 10}</h1>
+			<h1>{counter3 % 10}</h1>
+			<h1>{counter2 % 10}</h1>
+			<h1>{counter % 10}</h1>
+		</div>
 	);
 };
 
